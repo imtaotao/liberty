84bb3680326868d32ea561b2392fa5973cf7474c
imtaotao <dwchentao@163.com>
2019-06-20 16:40:56 +0800

    test: add test case

--- test/script/uitls.spec.js
+++ test/script/uitls.spec.js
@@ -1,5 +1,35 @@
+import * as _ from '../../src/utils'
+
 describe('utils method', () => {
-  it('test', () => {
-    expect(1).toBe(1)
+  it('real path', () => {
+    const cfg = {
+      envDir: '/',
+      envPath: '/test.js',
+    }
+    const a = _.realPath('./dev//a.js', cfg, {})
+    const b = _.realPath('./dev//../test/file.a.js', cfg, {})
+    const c = _.realPath('@A/a.json', cfg, {
+      exname: '.grs',
+      alias: { A: 'dev' }
+    })
+    const d = _.realPath('@A', cfg, {
+      exname: '.grs',
+      alias: { A: 'dev' },
+    })
+    expect(a.exname).toBe('.js')
+    expect(a.path).toBe('/dev/a.js')
+    expect(b.exname).toBe('.js')
+    expect(b.path).toBe('/test/file.a.js')
+    expect(c.exname).toBe('.json')
+    expect(c.path).toBe('/dev/a.json')
+    expect(d.exname).toBe('.grs')
+    expect(d.path).toBe('/dev.grs')
+  })
+
+  it('parent config', () => {
+    const res = _.getParentConfig('/', 'http://xx/dev/test/a.js')
+    expect(res.envPath).toBe('/')
+    expect(res.envDir).toBe('/dev/test')
+    expect(res.dirname).toBe('http://xx/dev/test')
   })
 })
