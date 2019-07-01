797730e6a0d24aec19968f491a7b95616703f13c
imtaotao <dwchentao@163.com>
2019-07-01 20:56:15 +0800

    fix: modify demo code

--- demo/index.js
+++ demo/index.js
@@ -1,20 +1,5 @@
-const Grass = require('@grass')
-const testJson = require('./test.json')
+require('./global.css')
 const UserList = require('./root/index')
 
-const setCss = () => {
-  const globalCss = require('./global.css')
-  const listCss = require('./list/style.css')
-  const changeUserCss = require('./change-user-infor/style.css')
-  const addUserCss = require('./add-user/style.css')
-  const rootCss = require('./root/style.css')
-
-  const styleNode = document.createElement('style')
-  styleNode.textContent = globalCss + listCss + changeUserCss + addUserCss + rootCss
-  document.getElementsByTagName('head')[0].appendChild(styleNode)
-}
-
-setCss()
-
-localStorage.setItem('User', testJson)
-Grass.mount(document.getElementById('root'), UserList)
+localStorage.setItem('User', require('./test.json'))
+UserList.$mount(document.getElementById('root'))
